d6214fe2442cce9522121f09f5fa81e25512292a
HalldorjonD <77391518+derhalljon@users.noreply.github.com>
2022-04-06 15:49:39 +0000

    Update dhj1micard.dart

--- lib/dhj1micard.dart
+++ lib/dhj1micard.dart
@@ -41,73 +41,85 @@ class miCardDHJ extends StatelessWidget {
                 ),
                 textAlign: TextAlign.center,
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                color: Colors.orange,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.white,
-                      size: 20.0,
+              Padding(
+                padding: EdgeInsets.only(top: 12.0),
+                child: Card(
+                  color: Colors.orange,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.phone,
+                          color: Colors.white,
+                          size: 20.0,
+                        ),
+                        SizedBox(width: 10.0),
+                        SelectableText(
+                          '+354 555 5555',
+                          style: TextStyle(
+                            fontFamily: 'SourceSansPro',
+                            color: Colors.white,
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      ],
                     ),
-                    SizedBox(width: 10.0),
-                    Text(
-                      '+354 555 5555',
-                      style: TextStyle(
-                        fontFamily: 'SourceSansPro',
-                        color: Colors.white,
-                        fontSize: 18.0,
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
+              Card(
                 color: Colors.orange,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email_rounded,
-                      color: Colors.white,
-                      size: 20.0,
-                    ),
-                    SizedBox(width: 10.0),
-                    Text(
-                      'myemail@myemail.com',
-                      style: TextStyle(
-                        fontFamily: 'SourceSansPro',
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email_rounded,
                         color: Colors.white,
-                        fontSize: 18.0,
+                        size: 20.0,
                       ),
-                    ),
-                  ],
+                      SizedBox(width: 10.0),
+                      SelectableText(
+                        'myemail@myemail.com',
+                        style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          color: Colors.white,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
+              Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 color: Colors.orange,
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.search_rounded,
-                      color: Colors.white,
-                      size: 20.0,
-                    ),
-                    SizedBox(width: 10.0),
-                    Text(
-                      'LinkedIn Profile',
-                      style: TextStyle(
-                        fontFamily: 'SourceSansPro',
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.search_rounded,
                         color: Colors.white,
-                        fontSize: 18.0,
+                        size: 20.0,
                       ),
-                    ),
-                  ],
+                      SizedBox(width: 10.0),
+                      SelectableText(
+                        //tried to turn this into a clickable link but couldn't
+                        //get it to work.
+                        'LinkedIn Profile',
+                        style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          color: Colors.white,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               )
             ],
